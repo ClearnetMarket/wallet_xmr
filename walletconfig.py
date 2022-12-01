d93f0e255b7692517705b60d4b116a9c35872401
Anekdotin <eddwinn@gmail.com>
2022-12-01 08:53:49 -0500

    Added config file for wallet

---
+++ walletconfig.py
@@ -0,0 +1,13 @@
+from decimal import Decimal
+
+digital_currency = 3
+# LIVE WALLET
+minconf = str(6)
+
+minamount = Decimal(0.00000001)
+maxamount = Decimal(5)
+
+rpcusername = 'rpcusername'
+rpcpassword = 'rpcpassword'
+
+url = "http://localhost:12388/json_rpc"
